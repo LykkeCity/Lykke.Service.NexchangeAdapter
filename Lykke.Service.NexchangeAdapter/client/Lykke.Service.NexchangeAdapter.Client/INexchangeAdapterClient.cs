0dbe4a1dae940dfa128d9e7473ea1bdf28ae9392
Sergey Polonsky <polonsky.sergey@hotmail.com>
2018-10-09 19:09:33 +0300

    Created solution based on template.

---
+++ Lykke.Service.NexchangeAdapter/client/Lykke.Service.NexchangeAdapter.Client/INexchangeAdapterClient.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+
+namespace Lykke.Service.NexchangeAdapter.Client
+{
+    /// <summary>
+    /// NexchangeAdapter client interface.
+    /// </summary>
+    [PublicAPI]
+    public interface INexchangeAdapterClient
+    {
+        // Make your app's controller interfaces visible by adding corresponding properties here.
+        // NO actual methods should be placed here (these go to controller interfaces, for example - INexchangeAdapterApi).
+        // ONLY properties for accessing controller interfaces are allowed.
+
+        /// <summary>Application Api interface</summary>
+        INexchangeAdapterApi Api { get; }
+    }
+}
---
+++ Lykke.Service.NexchangeAdapter/client/Lykke.Service.NexchangeAdapter.Client/NexchangeAdapterServiceClientSettings.cs
@@ -0,0 +1,14 @@
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Service.NexchangeAdapter.Client 
+{
+    /// <summary>
+    /// NexchangeAdapter client settings.
+    /// </summary>
+    public class NexchangeAdapterServiceClientSettings 
+    {
+        /// <summary>Service url.</summary>
+        [HttpCheck("api/isalive")]
+        public string ServiceUrl {get; set;}
+    }
+}
