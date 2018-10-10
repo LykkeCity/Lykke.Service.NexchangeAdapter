5fb6cb1a76e8d0a7c8436c86a3e7cb23de869430
Roman Chuprikov <paradise.develop@gmail.com>
2018-10-10 18:41:39 +0500

    ME contracts v1

---
+++ contract/Lykke.Service.NexchangeAdapter.Contract/Models/OrderModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lykke.Service.NexchangeAdapter.Contract.Enums;
+using ProtoBuf;
+
+namespace Lykke.Service.NexchangeAdapter.Contract.Models
+{
+    [ProtoContract]
+    public class OrderModel
+    {
+        [ProtoMember(1)]
+        public OrderType OrderType { get; set; }
+
+        [ProtoMember(2)]
+        public string Id { get; set; }
+
+        [ProtoMember(3)]
+        public string AssetPairId { get; set; }
+
+        [ProtoMember(4)]
+        public string WalletId { get; set; }
+
+        [ProtoMember(5)]
+        public OrderSide Side { get; set; }
+
+        [ProtoMember(6)]
+        public string Volume { get; set; }
+
+        [ProtoMember(7)]
+        public string RemainingVolume { get; set; }
+
+        [ProtoMember(8)]
+        public string Price { get; set; }
+
+        [ProtoMember(9)]
+        public OrderStatus Status { get; set; }
+
+        [ProtoMember(10)]
+        public string RejectReason { get; set; }
+
+        /// <summary>
+        /// Last status date
+        /// </summary>
+        [ProtoMember(11, DataFormat = DataFormat.WellKnown)]
+        public DateTime StatusDate { get; set; }
+
+        /// <summary>
+        /// Date from incoming request
+        /// </summary>
+        [ProtoMember(12, DataFormat = DataFormat.WellKnown)]
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Date of first processing in ME
+        /// </summary>
+        [ProtoMember(13, DataFormat = DataFormat.WellKnown)]
+        public DateTime Registered { get; set; }
+
+        [ProtoMember(14)]
+        public List<TradeModel> Trades { get; set; }
+    }
+}
---
+++ contract/Lykke.Service.NexchangeAdapter.Contract/Models/TradeModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lykke.Service.NexchangeAdapter.Contract.Enums;
+using ProtoBuf;
+
+namespace Lykke.Service.NexchangeAdapter.Contract.Models
+{
+    [ProtoContract]
+    public class TradeModel
+    {
+        [ProtoMember(1)]
+        public string TradeId { get; set; }
+
+        [ProtoMember(2)]
+        public string BaseAssetId { get; set; }
+
+        [ProtoMember(3)]
+        public string BaseVolume { get; set; }
+
+        [ProtoMember(4)]
+        public string Price { get; set; }
+
+        [ProtoMember(5, DataFormat = DataFormat.WellKnown)]
+        public DateTime Timestamp { get; set; }
+
+        [ProtoMember(6)]
+        public string OppositeOrderId { get; set; }
+
+        [ProtoMember(8)]
+        public string OppositeWalletId { get; set; }
+
+        [ProtoMember(9)]
+        public string QuotingAssetId { get; set; }
+
+        [ProtoMember(10)]
+        public string QuotingVolume { get; set; }
+
+        [ProtoMember(11)]
+        public int Index { get; set; }
+
+        [ProtoMember(12)]
+        public string AbsoluteSpread { get; set; } // ???
+
+        [ProtoMember(13)]
+        public string RelativeSpread { get; set; } // ???
+
+        [ProtoMember(14)]
+        public TradeRole Role { get; set; }
+    }
+}
